86644a580ae0079cf9b17b1edbfb7983627c40e9
Naveen-Ananthoju <140945142+Naveen-Ananthoju@users.noreply.github.com>
2023-08-03 19:47:56 +0530

    Add files via upload

---
+++ volume-create-delete.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#Action="Create"
+Action=${Action}
+if [ $Action == "Create" ]
+then
+i=0
+for i in {1..3}
+do
+echo "Creating Volume$i"
+aws ec2 create-volume --availability-zone us-east-1a --size 1
+done
+else
+#volumeswithsize1=$(aws ec2 describe-volumes --query 'Volumes[?Size==`1`].VolumeId' | tr -d '"' | tr -d ',')
+volumeswithsize1=$(aws ec2 describe-volumes --query 'Volumes[?Size==`1`].VolumeId' | jq -r '.[]')
+for vol in $volumeswithsize1
+do
+echo "Deleting volume: $vol"
+aws ec2 delete-volume --volume-id "$vol"
+done
+fi
+
+
